c3b92023d2d9429aa40bce80008e3ad1514cb4bb
MeJ <don.juncos@gmail.com>
2020-11-05 10:53:26 -0300

    Deploy on Heroku

--- web/src/main/java/academy/learnprogramming/interceptor/RequestInterceptor.java
+++ web/src/main/java/academy/learnprogramming/interceptor/RequestInterceptor.java
@@ -14,26 +14,26 @@ public class RequestInterceptor implements HandlerInterceptor {
 	public boolean preHandle(HttpServletRequest request,
 	                         HttpServletResponse response,
 	                         Object handler) throws Exception {
-	log.debug("preHandle method call P{}", handler);
-		log.debug("URL {}", request.getRequestURI());
+	// log.debug("preHandle method call P{}", handler);
+	// 	log.debug("URL {}", request.getRequestURI());
 		return true;
 	}
 
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
 
-		log.debug("postHandle method call P{}", handler);
-		log.debug("URL {}", request.getRequestURI());
-		log.debug("model {}", modelAndView.getModel());
-		log.debug("view = {}", modelAndView.getViewName());
+		// log.debug("postHandle method call P{}", handler);
+		// log.debug("URL {}", request.getRequestURI());
+		// log.debug("model {}", modelAndView.getModel());
+		// log.debug("view = {}", modelAndView.getViewName());
 		}
 
 	@Override
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
 	                            Exception ex) throws Exception {
 
-		log.debug("afterCompletion method call P{}", handler);
-		log.debug("URL {}", request.getRequestURI());
+		// log.debug("afterCompletion method call P{}", handler);
+		// log.debug("URL {}", request.getRequestURI());
 
 	}
 }
